be6d4568eea6ce6c111e473e1eb0c2505f16dc16
dragan023 <dragan.carkic@vivifyideas.com>
2020-01-31 19:48:36 +0100

    Added styles for homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,37 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <style>
+            body  {
+                background: #123;
+                margin: 0;
+                font-size: 2rem;
+                font-family: 'Nunito';
+                text-align: center;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                height: 100vh;
+            }
+
+            h1, h5 {
+                color: orange;
+                margin-top: 0;
+                margin-bottom: 0;
+            }
+
+            a, ol {
+                color: #fff;
+                text-decoration: none;
+            }
+
+            a:hover {
+                color: orange;
+                transition: color 0.3s;
+            }
+        </style>
+
+
     </head>
     <body>
         <h1>Welcome {{ $name }}!</h1>
